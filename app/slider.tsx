7dd20c42e8f005357a44b1632c40ff56858c4e2b
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-10-08 20:45:40 +0200

    improve ranger slider docs

--- app/slider.tsx
+++ app/slider.tsx
@@ -17,67 +17,193 @@ export const [useSlider, SliderProvider, SliderContext] = createHookContext(
 export const SliderConsumer = SliderContext.Consumer;
 
 export const SliderRoot = (props: ComponentProps<'div'>) => (
-  <div {...mergeProps(useSlider().getRootProps(), props)} />
+  <div
+    {...mergeProps(
+      useSlider().getRootProps(),
+      {
+        className: ['flex', 'flex-col', 'gap-1', 'w-full'].join(' '),
+      },
+      props
+    )}
+  />
 );
 
 export const SliderControl = (props: ComponentProps<'div'>) => (
-  <div {...mergeProps(useSlider().getControlProps(), props)} />
+  <div
+    {...mergeProps(
+      useSlider().getControlProps(),
+      {
+        className: [
+          'relative',
+          'flex',
+          'items-center',
+          'cursor-pointer',
+          'data-[focus]:ring-2',
+          'data-[focus]:ring-ring',
+          'data-[focus]:ring-offset-2',
+          'data-[focus]:rounded-sm',
+          'h-5',
+        ].join(' '),
+      },
+      props
+    )}
+  />
 );
 
 export const SliderTrack = (props: ComponentProps<'div'>) => (
-  <div {...mergeProps(useSlider().getTrackProps(), props)} />
+  <div
+    {...mergeProps(
+      useSlider().getTrackProps(),
+      {
+        className: [
+          'bg-input',
+          'rounded-full',
+          'overflow-hidden',
+          'flex-1',
+          'h-2',
+        ].join(' '),
+      },
+      props
+    )}
+  />
 );
 
 export const SliderRange = (props: ComponentProps<'div'>) => (
-  <div {...mergeProps(useSlider().getRangeProps(), props)} />
+  <div
+    {...mergeProps(
+      useSlider().getRangeProps(),
+      { className: ['bg-foreground', 'h-2'].join(' ') },
+      props
+    )}
+  />
+);
+
+export const SliderMarkerGroup = (props: ComponentProps<'div'>) => (
+  <div
+    {...mergeProps(
+      useSlider().getMarkerGroupProps(),
+      {
+        className: [
+          'absolute',
+          'text-sm',
+          'before:h-1',
+          'before:-top-3',
+          'before:w-1',
+        ].join(' '),
+      },
+      props
+    )}
+  />
+);
+
+export const SliderMarker = ({
+  value,
+  ...props
+}: ComponentProps<'div'> & slider.MarkerProps) => (
+  <div
+    {...mergeProps(
+      useSlider().getMarkerProps({ value }),
+      {
+        className: [
+          '[--before-background:hsl(var(--foreground))]',
+          'text-sm',
+          'text-muted-foreground',
+          'before:bg-background',
+          'before:rounded-full',
+          'before:content-[""]',
+          'before:block',
+          'before:left-1/2',
+          'before:relative',
+          'before:-translate-x-1/2',
+          'before:h-1',
+          'before:-top-3',
+          'before:w-1',
+        ].join(' '),
+      },
+      props
+    )}
+  />
 );
 
 export const SliderThumb = ({
   index,
   ...props
 }: ComponentProps<'div'> & slider.ThumbProps) => (
-  <div {...mergeProps(useSlider().getThumbProps({ index }), props)} />
+  <div
+    {...mergeProps(
+      useSlider().getThumbProps({ index }),
+      {
+        className: [
+          'bg-background',
+          'border-2',
+          'border-foreground',
+          'rounded-full',
+          'outline-none',
+          'shadow-sm',
+          'z-[1]',
+          'data-[focus]:ring-2',
+          'data-[focus]:border-ring',
+          'data-[focus]:ring-foreground',
+          'size-5',
+        ].join(' '),
+      },
+      props
+    )}
+  />
 );
 
 export const SliderHiddenInput = ({
   index,
   ...props
 }: ComponentProps<'div'> & slider.ThumbProps) => (
-  <div {...mergeProps(useSlider().getHiddenInputProps({ index }), props)} />
+  <div
+    {...mergeProps(
+      useSlider().getHiddenInputProps({ index }),
+      { className: '' },
+      props
+    )}
+  />
 );
 
 export const SliderLabel = (props: ComponentProps<'label'>) => (
-  <label {...mergeProps(useSlider().getLabelProps(), props)} />
+  <label
+    {...mergeProps(
+      useSlider().getLabelProps(),
+      { className: 'font-medium text-sm' },
+      props
+    )}
+  />
 );
 
 export const SliderValueText = (props: ComponentProps<'output'>) => (
-  <output {...mergeProps(useSlider().getValueTextProps(), props)} />
+  <output
+    {...mergeProps(useSlider().getValueTextProps(), { className: '' }, props)}
+  />
 );
 
 export function RangeSlider() {
-  const [state, send] = useMachine(
-    slider.machine({
-      id: '1',
-      name: 'quantity',
-      value: [10, 60],
-    })
-  );
-
-  const api = slider.connect(state, send, normalizeProps);
-
   return (
     <SliderProvider id={useId()} name="quantity" value={[10, 60]}>
-      <SliderRoot>
+      <SliderRoot className="min-w-[200px]">
         <SliderControl>
           <SliderTrack>
             <SliderRange />
           </SliderTrack>
-          {api.value.map((_, index) => (
-            <SliderThumb key={index} index={index}>
-              <SliderHiddenInput index={index} />
-            </SliderThumb>
-          ))}
+          <SliderConsumer>
+            {(api) =>
+              api.value.map((_, index) => (
+                <SliderThumb key={index} index={index}>
+                  <SliderHiddenInput index={index} />
+                </SliderThumb>
+              ))
+            }
+          </SliderConsumer>
         </SliderControl>
+        <SliderMarkerGroup>
+          <SliderMarker value={25}>25</SliderMarker>
+          <SliderMarker value={50}>50</SliderMarker>
+          <SliderMarker value={75}>75</SliderMarker>
+        </SliderMarkerGroup>
       </SliderRoot>
     </SliderProvider>
   );
@@ -86,7 +212,7 @@ export function RangeSlider() {
 export function Slider() {
   return (
     <SliderProvider id={useId()} name="quantity" value={[10, 60]}>
-      <SliderRoot>
+      <SliderRoot className="min-w-[200px]">
         <div>
           <SliderLabel>Slider Label</SliderLabel>
           <SliderConsumer>
@@ -107,6 +233,11 @@ export function Slider() {
             }
           </SliderConsumer>
         </SliderControl>
+        <SliderMarkerGroup>
+          <SliderMarker value={25}>25</SliderMarker>
+          <SliderMarker value={50}>50</SliderMarker>
+          <SliderMarker value={75}>75</SliderMarker>
+        </SliderMarkerGroup>
       </SliderRoot>
     </SliderProvider>
   );
