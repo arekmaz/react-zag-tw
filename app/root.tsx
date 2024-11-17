db0044461d5792cbece261ea398a3641cfc3bbec
Arkadiusz Mazurkiewicz <mazurkiewiczar@gmail.com>
2024-11-17 15:21:24 +0100

    --wip-- [skip ci]

--- app/root.tsx
+++ app/root.tsx
@@ -24,14 +24,14 @@ export const links: LinksFunction = () => [
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className='dark' >
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body className="font-sans">
+      <body className="font-sans dark">
         {children}
         <ScrollRestoration />
         <Scripts />
